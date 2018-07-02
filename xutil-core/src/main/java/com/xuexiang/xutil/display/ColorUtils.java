c0d2439d238928fcf4a46aab84159d3e4fdb3a33
薛翔 <xuexiangjys@163.com>
2018-07-02 15:10:18 +0800

    增加Drawable生成工具类

--- xutil-core/src/main/java/com/xuexiang/xutil/display/ColorUtils.java
+++ xutil-core/src/main/java/com/xuexiang/xutil/display/ColorUtils.java
@@ -88,6 +88,15 @@ public final class ColorUtils {
      * 加深颜色
      *
      * @param color  需要加深的颜色
+     */
+    public static int darker(int color) {
+        return darker(color, 0.8F);
+    }
+
+    /**
+     * 加深颜色
+     *
+     * @param color  需要加深的颜色
      * @param factor The factor to darken the color.
      * @return darker version of specified color.
      */
@@ -124,6 +133,21 @@ public final class ColorUtils {
                         - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color))
                         / 255;
         return darkness >= 0.5;
+    }
+
+    /**
+     * 颜色选择器
+     *
+     * @param pressedColor 按下的颜色
+     * @param normalColor  正常的颜色
+     * @return 颜色选择器
+     */
+    public static ColorStateList getColorStateList(int pressedColor, int normalColor) {
+        //其他状态默认为白色
+        return new ColorStateList(
+                new int[][]{{android.R.attr.state_enabled,
+                        android.R.attr.state_pressed}, {android.R.attr.state_enabled}, {}},
+                new int[]{pressedColor, normalColor, Color.WHITE});
     }
 
     public static ColorStateList generateThumbColorWithTintColor(final int tintColor) {
@@ -218,16 +242,77 @@ public final class ColorUtils {
     }
 
     /**
-     * 获取随机颜色
+     * 按条件的到随机颜色
      *
-     * @return
+     * @param alpha 透明
+     * @param lower 下边界
+     * @param upper 上边界
+     * @return 颜色值
+     */
+
+    public static int getRandomColor(int alpha, int lower, int upper) {
+        return new RandomColor(alpha, lower, upper).getColor();
+    }
+
+    /**
+     * @return 获取随机色
      */
     public static int getRandomColor() {
-        Random random = new Random();
-        int r = random.nextInt(256);
-        int g = random.nextInt(256);
-        int b = random.nextInt(256);
-        return Color.rgb(r, g, b);
+        return new RandomColor(255, 0, 255).getColor();
+    }
+
+    /**
+     * 随机颜色
+     */
+    public static class RandomColor {
+        int alpha;
+        int lower;
+        int upper;
+
+        RandomColor(int alpha, int lower, int upper) {
+            if (upper <= lower) {
+                throw new IllegalArgumentException("must be lower < upper");
+            }
+            setAlpha(alpha);
+            setLower(lower);
+            setUpper(upper);
+        }
+
+        public int getColor() {
+            //随机数是前闭  后开
+            int red = getLower() + new Random().nextInt(getUpper() - getLower() + 1);
+            int green = getLower() + new Random().nextInt(getUpper() - getLower() + 1);
+            int blue = getLower() + new Random().nextInt(getUpper() - getLower() + 1);
+            return Color.argb(getAlpha(), red, green, blue);
+        }
+
+        public int getAlpha() {
+            return alpha;
+        }
+
+        public void setAlpha(int alpha) {
+            if (alpha > 255) alpha = 255;
+            if (alpha < 0) alpha = 0;
+            this.alpha = alpha;
+        }
+
+        int getLower() {
+            return lower;
+        }
+
+        void setLower(int lower) {
+            if (lower < 0) lower = 0;
+            this.lower = lower;
+        }
+
+        int getUpper() {
+            return upper;
+        }
+
+        void setUpper(int upper) {
+            if (upper > 255) upper = 255;
+            this.upper = upper;
+        }
     }
 
 
